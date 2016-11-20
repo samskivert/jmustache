49720df4ead6445bd6c4c4497e872dc24dd7435f
Michael Bayne <mdb@fb.com>
2016-11-20 11:33:50 -0800

    Use shipped GWT 2.8.0 (yay).
    Also made main source target 1.8 (from 1.6) and nixed some GWT test
    cruft.

--- src/test/java/com/samskivert/mustache/GwtTestMustache.java
+++ src/test/java/com/samskivert/mustache/GwtTestMustache.java
@@ -20,30 +20,4 @@ public class GwtTestMustache extends SharedTests
     public String getModuleName () {
         return "com.samskivert.Mustache";
     }
-
-    // @Test public void testSimpleVariable () {
-    //     test("bar", "{{foo}}", context("foo", "bar"));
-    // }
-
-    // @Test public void testPrimitiveArrayVariable () {
-    //     test("1234", "{{#foo}}{{this}}{{/foo}}", new Object() {
-    //         int[] getFoo () { return new int[] { 1, 2, 3, 4 }; }
-    //     });
-    // }
-
-    // protected void test (String expected, String template, Object ctx) {
-    //     test(Mustache.compiler(), expected, template, ctx);
-    // }
-
-    // protected void test (Mustache.Compiler compiler, String expected, String template, Object ctx) {
-    //     assertEquals(expected, compiler.compile(template).execute(ctx));
-    // }
-
-    // protected Object context (Object... data) {
-    //     Map<String, Object> ctx = new HashMap<String, Object>();
-    //     for (int ii = 0; ii < data.length; ii += 2) {
-    //         ctx.put(data[ii].toString(), data[ii+1]);
-    //     }
-    //     return ctx;
-    // }
 }
